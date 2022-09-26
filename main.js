b3472ae8f56cde32fa418e100e5405d4990e6724
563795261 <114324702+563795261@users.noreply.github.com>
2022-09-26 12:32:05 +0800

    Update main.js

--- main.js
+++ main.js
@@ -66,8 +66,26 @@ const main = async () => {
 
 main()
 
+from datetime import date, datetime
+import math
+from wechatpy import WeChatClient
+from wechatpy.client.api import WeChatMessage, WeChatTemplate
+import requests
+import os
+import random
+
  def get_words():
   words = requests.get("https://api.shadiao.pro/chp")
   if words.status_code != 200:
     return get_words()
   return words.json()['data']['text']
+def get_random_color():
+  return "#%06x" % random.randint(0, 0xFFFFFF)
+client = WeChatClient(app_id, app_secret)
+
+wm = WeChatMessage(client)
+wea, temperature = get_weather()
+data = {"words":{"value":get_words(), "color":get_random_color()}}
+res = wm.send_template(user_id, template_id, data)
+print(res)
+
