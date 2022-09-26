89075e7194f06a578504580586b758233070bdb3
563795261 <114324702+563795261@users.noreply.github.com>
2022-09-26 10:44:46 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -22,7 +22,7 @@ export const config = {
     // 生日，修改名字为对应需要显示的名字，如果生日为农历， type为 r
     birthdays: [
       {"name": "彭先生", "year": "2022", "date": "11-25", "type": 'new'},
-      {"name": "詹太太", "year": "2022", "date": "12-27", "type": 'new'},
+      {"name": "詹太太", "year": "1998", "date": "11-02", "type": 'r'},
     ],
     }
 
